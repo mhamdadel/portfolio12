7473b368839beb64dd7a335651a3e502ef343642
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-02-11 10:18:31 +0200

    add scss files, html, compile to css, add section [navbar - header - contect me - portfilio [cards] ]

---
+++ README.md
@@ -0,0 +1,1 @@
+# portfolio Auther : Muhammed Adel
---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+document.onscroll = function(e){
+    let contactMe = document.getElementById("contact-me");
+    let bodyElements = document.body.children;
+    let navbar = document.getElementById("navbar");
+    let navList = document.getElementById("nav-list");
+    if(navbar.offsetTop + navbar.offsetHeight > document.documentElement.scrollTop){
+        navbar.style.position = "static";
+        navbar.classList.remove("nav-dark-bg");
+    }else{
+        navbar.style.position = "fixed";
+        navbar.style.zIndex = "65000";
+        navbar.classList.add("nav-dark-bg");
+    }
+
+    [...bodyElements].forEach(function(el,index){
+        const elementTop = el.offsetTop;
+        const elementTopHeight = el.offsetTop + el.offsetHeight;
+        let whereAreYou = document.documentElement.scrollTop;
+        if(whereAreYou >= elementTop && whereAreYou <= elementTopHeight){
+            [...navList.children].forEach(function(ele, ind){
+                ele.classList.remove("active");
+            });
+            [...navList.children][index-1]?.classList.add("active");
+        }
+    });
+}
